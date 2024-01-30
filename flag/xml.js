85d30ed2dfe5a5429b08d813ceb484b43bd04e98
Meenajayaraj <sundharaj2517@gmail.com>
2024-01-30 11:22:34 +0530

    task-3

--- flag/xml.js
+++ flag/xml.js
@@ -2,12 +2,23 @@ const URL = "https://restcountries.com/v3.1/all";
 
 const xml =new XMLHttpRequest();
 xml.open("GET",URL);
-xml.send();
-
-xml.onreadystatechange = () =>
+xml.responseType = "json";
+  
 {
-    console.log(xml.readystate);
-};
-xml.onload = () => {
-    console.log(JSON.parse(xml.response));
-};
+  xml.onload = () => {
+    const data = xml.response;
+    //const datas = data;
+    getData(data);
+     };
+    
+  xml.send();
+  }
+  getUSDollar();
+  
+  function getData (data){
+    data.forEach ((country) => {
+   console.log ("Name: " + country.name.common);
+   console.log ("Region : "+country.region);
+   console.log("Sub Region : "+country.subregion);
+   console.log("Population : "+country.population);
+          });
