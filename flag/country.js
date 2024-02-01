f4d27fc176317d8ceb0f3477934705d6071affdf
sundharaj2517@gmail.com <sundharaj2517@gmail.com>
2024-02-01 21:10:35 +0530

    task-3

--- flag/country.js
+++ flag/country.js
@@ -1,26 +1,40 @@
-const getUSDollar = () => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://restcountries.com/v3.1/all", true);
+let getflag = () => {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://restcountries.com/v3.1/all", true);
+
+  xhr.responseType = "json";
+
+
+xhr.onload = () => {
+  let data = xhr.response;
   
-    xhr.responseType = "json";
   
+  getData(data);
+   };
   
-  xhr.onload = () => {
-    const data = xhr.response;
-    //const datas = data;
-    getData(data);
-     };
+xhr.send();
+}
+getflag();
+
+function getData (data){
+  const container= document.createElement("div");
+container.className ="container";
+document.body.append(container);
+
+const row=document.createElement("div");
+row.className ="row";
+document.body.append (row);
+  
+  data.forEach ((country) => {
     
-  xhr.send();
-  }
-  getUSDollar();
-  
-  function getData (data){
-    data.forEach ((country) => {
-   console.log ("Name: " + country.name.common);
-   console.log ("Region : "+country.region);
-   console.log("Sub Region : "+country.subregion);
-   console.log("Population : "+country.population);
-          });
-  }
-  
+const txtFlag= document.createElement ('img');
+txtFlag.className = 'info';
+txtFlag.src = country.flags.png;
+document.body.appendChild(txtFlag);
+
+const txtName= document.createElement ('h3');
+txtName.innerHTML = country.name.common;
+document.body.appendChild(txtName);
+ 
+        });
+}
