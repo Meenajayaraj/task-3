c21d1c2904ad147aef367a111688aa748f5f98d7
sundharaj2517@gmail.com <sundharaj2517@gmail.com>
2024-02-02 20:47:23 +0530

    task-3

--- compare/script.js
+++ compare/script.js
@@ -9,7 +9,25 @@ let object1 = {
     country:'india',
     name: 'Person1',
   };
+      let flag = true;
+      if(Object.keys(object1).length== Object.keys(object2))
+      {
+        for ( key in object1)
+        {
+          if(object1[key]==object2[key])
+          {
+            continue;
+          }
+          else
+          {
+            flag = false;
+            break;
+          }
+        } 
+      }
+      else
+      {
+        flag=false;
+      }
+      console.log("object is"+ flag);
       
-  JSON.stringify(object1) === JSON.stringify(object2)
-      
-  _.isEqual(object1, object2)
