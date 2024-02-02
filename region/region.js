e2e7a4ce03673970e90b5581fcf943dc91ce3d97
sundharaj2517@gmail.com <sundharaj2517@gmail.com>
2024-02-02 08:56:16 +0530

    task-3

--- region/region.js
+++ region/region.js
@@ -1,17 +1,16 @@
  const xhr = new XMLHttpRequest();
     const URL = "https://restcountries.com/v3.1/all";
     xhr.open("GET",URL);
-    //xhr.responseType = "json";
+    xhr.responseType = "json";
     xhr.send();
 
 }
-
 xhr.onload=()=>{
-   for(let rcf of (JSON.parse(xhr.response))){
-    console.log("NAME :" ,rcf.name.common);
-    console.log("REGION :" ,rcf.region);
-    console.log("SUBREGION :" ,rcf.subregion);
-    console.log("POPULATION :" ,rcf.population);
+   for(let reg of (JSON.parse(xhr.response))){
+    console.log("NAME :" ,reg.name.common);
+    console.log("REGION :" ,reg.region);
+    console.log("SUBREGION :" ,reg.subregion);
+    console.log("POPULATION :" ,reg.population);
    }
 };
 }
